0a233654d5e7e19dfaf209164c0564d738932420
Andrew <andrewhunter542@gmail.com>
2022-09-21 11:47:53 -0400

    added docstring to review class

--- review.py
+++ review.py
@@ -3,17 +3,27 @@ from user import User
 from listing import Listing
 
 class Review():
-    """
+    """A review object that represents a review posted by a guest
 
+    params:
+    - id: the review id (unique), used to identify the review
+    - date_posted: the date the review was posted
+    - posting_user: the user that left the review
+    - listing: the listing that the reviewing user stayed at/purchased
+    - rating: the rating out of 5 stars the user left
+    - comment: the comment the user left on the review
+
+    Note: Checking that the user is a verified guest to the listing 
+    has not been implemented yet
     """
     
     def __init__(self):
-        self.id = None # random unique int, implement later
+        self.id = None
         self.date_posted = None
         self.posting_user = None
         self.listing = None
         self.rating = None
-        self.description = None
+        self.comment = None
 
     @property
     def __str__(self):
@@ -66,12 +76,12 @@ class Review():
         self._rating = rating
 
     @property
-    def description(self):
-        return self._description
+    def comment(self):
+        return self._comment
 
-    @description.setter
-    def description(self, description):
-        self._description = description
+    @comment.setter
+    def comment(self, comment):
+        self._comment = comment
 
 
 
