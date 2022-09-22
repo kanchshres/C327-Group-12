c67b4674e9c131964d90d918a41e3f05a015516b
KazSusilo <20ARS9@queensu.ca>
2022-09-21 20:46:29 -0400

    Integrated listing.py
    Integrated listing.py such that it can work with the rest of the files.
    
    Added more tests cases for Reviews.

--- listing.py
+++ listing.py
@@ -1,9 +1,10 @@
 #listing.py
 from enum import Enum, unique
 from user import User
+from review import Review
 
 class Listing:
-	""" Object representation of a digital Listing
+    """ Object representation of a digital Listing
 
 	params:
 	- title: Title of listing (string)
@@ -13,86 +14,71 @@ class Listing:
 	- description: A short description (string)
 	- reviews: A list of reviews associates with the listing (list[Review])
 	"""
-
-	def __init__(self):
-		self._title = ""                     
-		self._address = ""
-		self._price = 0.0
-		self._description = ""
-		self._seller = None # Seller should be of type User
-		#self._reviews: list[Review] = []
-
-	@property
-	def title(self):
-		return self._title
+    def __init__(self):
+        self._title = ""
+        self._address = ""
+        self._price = 0.0
+        self._description = ""
+        self._seller = User()
+        self._reviews: list[Review] = []
 
 
-	@title.setter
-	def title(self, subject):
-		self._title = subject
-
-
-	@property
-	def address(self):
-		return self._address
-
-
-	@address.setter
-	def address(self, location):
-		self._address = location
-
-
-	@property
-	def price(self):
-		return self._price
-
-
-	@price.setter
-	def price(self, value):
-		self._price = value
-
-
-	@property
-	def description(self):
-		return self._description
-
-
-	@description.setter
-	def description(self, text):
-		self._description = text
-
-
-	@property
-	def seller(self):
-		return self._seller
-
-
-	@seller.setter
-	def seller(self, person):
-		self._seller = person
-
-
-    # Missing Review object
-	"""
     @property
-	def reviews(self) -> list[Review]
-		return self._reviews
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, subject):
+        self._title = subject
 
 
-	@reviews.setter
-	def reviews(self, comments:list[Review]):
-		if all(isinstance(i, list[Review]) for i in comments):
-			self._reviews = comments
-		else:
-			raise ValueError("comments must be a list of Review objects")
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, location):
+        self._address = location
 
 
-	#def add_review(self, review:Review):
-		if not isinstance(review, Review):
-			raise ValueError("review must be a Review object")
-		self._reviews.append(review)
-    """
+    @property
+    def price (self):
+        return self._price
 
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, text):
+        self._description = text
+
+
+    @property
+    def seller(self):
+        return self._seller
+
+    @seller.setter
+    def seller(self, person):
+        self._seller = person
+
+    
+    @property
+    def reviews(self) -> 'list[Review]':
+        return self._reviews
+    
+    @reviews.setter
+    def reviews(self, comments: 'list[Review]'):
+        self._reviews = comments
+    
+    def add_review(self, review: 'Review'):
+        self._reviews.append(review)
+        
 
 # Test Code #
 if (__name__ == "__main__"):
@@ -105,7 +91,9 @@ if (__name__ == "__main__"):
     print(obj.price)
     print("description: " + obj.description)
     print("Seller: ", end = "")
-    print(obj.seller)
+    print(obj.seller.username)
+    print("Reviews: ", end = "")
+    print(obj.reviews)
 
     
     # Testing param maniupulation #
@@ -113,8 +101,14 @@ if (__name__ == "__main__"):
     obj.address = "Queen's University"
     obj.price = 8000.57
     obj._description = "Shittiest school to ever exist"
-    # bob = User()
-    obj.seller = "Bob" # Should be an object of type User
+    obj.seller.username = "bob"
+    r = []
+    r1 = Review()
+    r.append(r1)
+    obj.reviews = r
+    r2 = Review()
+    obj.add_review(r2)
+    
 
     print("")
     print("Listing Info")
@@ -124,4 +118,6 @@ if (__name__ == "__main__"):
     print(obj.price)
     print("description: " + obj.description)
     print("Seller: ", end = "")
-    print(obj.seller)
+    print(obj.seller.username)
+    print("Reviews: ", end = "")
+    print(obj.reviews)
