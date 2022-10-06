92696cd514363ae06d5b4d56d194c27b6fd6779b
Kanchan <shrestha.kanch@gmail.com>
2022-10-06 14:22:44 -0400

    Created register function and implemented tests for it

--- qbay/user.py
+++ qbay/user.py
@@ -8,6 +8,11 @@ if TYPE_CHECKING:
     from qbay.review import Review
 
 
+"""
+This file defines the User profile class
+"""
+
+
 class User():
     """ Object representation of a user's account
 
@@ -16,6 +21,8 @@ class User():
     - username: Username for user's account
     - email: Email associated with account
     - password: Password associated with account
+    - postal code: Postal code of user
+    - billing address: Billing address of user
     - wallet: Wallet object associated with account
     - review: All the reviews the user has created
     """
@@ -58,14 +65,7 @@ class User():
 
     @email.setter
     def email(self, email: str):
-        # Check if email is valid
-        regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+'
-                           '@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
-        if (re.fullmatch(regex, email)):
-            self._email = email
-        else:
-            raise ValueError("Not a valid email address.")
+        self._email = email
 
     @property
     def password(self) -> str:
@@ -102,3 +102,91 @@ class User():
 
     def add_review(self, review: 'Review'):
         self._reviews.append(review)
+    
+    @property
+    def postal_code(self):
+        return self._postal_code
+    
+    @postal_code.setter
+    def postal_code(self, pos_code: str):
+        self._postal_code = pos_code
+
+    @property
+    def billing_address(self):
+        return self._billing_address
+    
+    @billing_address.setter
+    def billing_address(self, bill_addr: str):
+        self._billing_address = bill_addr
+
+    
+def register(name, email, password):
+    """ Register a new user
+
+    params:
+        name (string):     user name
+        email (string):    user email
+        password (string): user password
+    
+    Raises:
+        ValueError: When any requirements are not met
+
+    Returns:
+        True if registration succeeded, otherwise False
+    """
+
+    # R1-1: Email and Password cannot be empty
+    if email == "" or password == "":
+        return False
+        
+    # R1-3: Valid email addr-spec
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+'
+                       '@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if not (re.fullmatch(regex, email)):
+        return False
+
+    # R1-4: Password complexity requirements
+    if len(password) < 6:
+        return False
+    if not (any(c.isupper() for c in password)):
+        return False
+    if not (any(c.islower() for c in password)):
+        return False
+    if not (any(not c.isalnum() for c in password)):
+        return False
+
+    # R1-5: Username specific requirements
+    if name == "":
+        return False
+    if name[0] == " " or name[-1] == " ":
+        return False
+    if any(not (c.isalnum() or c == " ") for c in name):
+        return False
+    
+    # R1-6: Username length requirements
+    if not (2 < len(name) < 20):
+        return False
+    
+    # R1-7: Email cannot be previously used
+    # need database for rest
+
+    # existed = User.query.filter_by(email=email).all()
+    # if len(existed) > 0:
+    #     return False
+    # user = User(username=name, email=email, password=password)
+    
+    # R1-2: User is identified by unique ID
+    # user.id = 
+
+    # R1-8: Billing address is empty
+    # R1-9: Postal code is empty
+
+    # R1-10: Balance is 100 at initialization
+    # user.wallet.balance = 100
+
+    # # add it to current database session
+    # db.session.add(user)
+    # # save user object
+    # db.session.commit()
+
+    return True
