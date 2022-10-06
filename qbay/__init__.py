e997f92133bb14505dda0b6f3dae773b64ddf3a5
ShuviSchwarze <shuvischwarze@gmail.com>
2022-10-06 19:14:03 -0400

    Fixing import errors

--- qbay/__init__.py
+++ qbay/__init__.py
@@ -7,8 +7,4 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 
-from . import database, user, listing, wallet, review, transaction, booking
-
 from qbay.database import db
-
-db.create_all()
