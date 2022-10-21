193319677402622c6d3463e6d9bf703f3708b138
Kanchan <shrestha.kanch@gmail.com>
2022-10-20 20:01:12 -0400

    Added more specific error messages to failed user registration.

--- qbay/controllers.py
+++ qbay/controllers.py
@@ -38,44 +38,9 @@ def authenticate(inner_function):
     return wrapped_inner
 
 
-@app.route('/login', methods=['GET'])
-def login_get():
-    return render_template('login.html', message='Please login')
-
-
-@app.route('/login', methods=['POST'])
-def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    user = User.login(email, password)
-    if user:
-        session['logged_in'] = user.email
-        """
-        Session is an object that contains sharing information 
-        between a user's browser and the end server. 
-        Typically it is packed and stored in the browser cookies. 
-        They will be past along between every request the browser made 
-        to this services. Here we store the user object into the 
-        session, so we can tell if the client has already login 
-        in the following sessions.
-        """
-        # success! go back to the home page
-        # code 303 is to force a 'GET' request
-        return redirect('/', code=303)
-    else:
-        return render_template('login.html', message='login failed')
-
-
 @app.route('/')
 @authenticate
 def home(user):
-    # authentication is done in the wrapper function
-    # see above.
-    # by using @authenticate, we don't need to re-write
-    # the login checking code all the time for other
-    # front-end portals
-
-    # some fake product data
     products = [
         {'name': 'product 1', 'price': 10},
         {'name': 'product 2', 'price': 20}
@@ -95,25 +60,25 @@ def register_post():
     name = request.form.get('name')
     password = request.form.get('password')
     password2 = request.form.get('password2')
-    error_message = None
+    error_msg = None
 
     if password != password2:
-        error_message = "The passwords do not match"
+        error_msg = "The passwords do not match, please retry."
     else:
-        # use backend api to register the user
-        success = User.register(name, email, password)
-        if not success:
-            error_message = "Registration failed."
-    # if there is any error messages when registering new user
-    # at the backend, go back to the register page.
-    if error_message:
-        return render_template('register.html', message=error_message)
+        # Create using backend api where it'll create only after checking if
+        # each parameter is valid
+        user = User.register(name, email, password)
+        if not user:
+            error_msg = "Registration failed!"
+            if (not User.valid_email(email)):
+                error_msg += " Improper email."
+            if (not User.valid_password(password)):
+                error_msg += " Improper password."
+            if (not User.valid_username(name)):
+                error_msg += " Improper username."
+    # If any error messages are encountered registering new user
+    # then go back to the register page.
+    if error_msg:
+        return render_template('register.html', message=error_msg)
     else:
         return redirect('/login')
-
-
-@app.route('/logout')
-def logout():
-    if 'logged_in' in session:
-        session.pop('logged_in', None)
-    return redirect('/')
