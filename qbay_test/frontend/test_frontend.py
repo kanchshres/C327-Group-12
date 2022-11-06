20b83afcc6b980473993f34ddddbe67049abb840
Kanchan <shrestha.kanch@gmail.com>
2022-11-06 16:00:06 -0500

    Added two test cases for Updating Listing page

--- qbay_test/frontend/test_frontend.py
+++ qbay_test/frontend/test_frontend.py
@@ -42,6 +42,7 @@ class FrontEndTests(BaseCase):
         self.assert_element("#message")
         self.assert_text("Registration failed.", "#message")
 
+        # Password cannot be empty
         self.type("#username", "Test Three")
         self.type("#email", "test01@test.com")
         self.click('input[type="submit"]')
@@ -97,4 +98,76 @@ class FrontEndTests(BaseCase):
         self.assert_element("#welcome-header")
         self.assert_text("Please login below", "#welcome-header")
 
-    
+    def test_listing_update_fail(self, *_):
+        """
+        Testing R5-2:
+        Price can only be increased.
+        """
+        # Input coverage testing by Input Partitioning
+
+        # Price decreased.
+        self.open(base_url + '/register')
+        self.type("#email", "updatelisting01@test.com")
+        self.type("#username", "Listing Update 01")
+        self.type("#password", "Onetwo!")
+        self.type("#password2", "Onetwo!")
+        self.click('input[type="submit"]')
+
+        self.open(base_url + '/login')
+        self.type("#email", "updatelisting01@test.com")
+        self.type("#password", "Onetwo!")
+        self.click('input[type="submit"]')
+
+        self.click_link("Create Listing")
+        self.type("#title", "4 bed 2 bath")
+        self.type("#description", "This is a lovely place with 4 beds 2 bath")
+        self.type("#price", 100)
+        self.click('input[type="submit"]')
+
+        self.click_link("Update Your Listings")
+        self.click('input[type="radio"]')
+        self.click('input[type="submit"]')
+        
+        self.type("#price", 49.99)
+        self.click('input[type="submit"]')
+        self.assert_element("#messages")
+        self.assert_text("Invalid Price: 49.99", "#messages")
+
+        # Price increased
+        self.type("#price", 149.99)
+        self.click('input[type="submit"]')
+        self.assert_element("#messages")
+        self.assert_text("Price updated successfully: 149.99", "#messages")
+
+    def test_listing_update_success(self, *_):
+        # Output coverage testing
+
+        # Outputs newly updated listing
+        self.open(base_url + '/register')
+        self.type("#email", "updatelisting02@test.com")
+        self.type("#username", "Listing Update 02")
+        self.type("#password", "Onetwo!")
+        self.type("#password2", "Onetwo!")
+        self.click('input[type="submit"]')
+
+        self.open(base_url + '/login')
+        self.type("#email", "updatelisting02@test.com")
+        self.type("#password", "Onetwo!")
+        self.click('input[type="submit"]')
+
+        self.click_link("Create Listing")
+        self.type("#title", "5 bed 3 bath")
+        self.type("#description", "This is a lovely place with 5 beds 3 bath")
+        self.type("#price", 499.99)
+        self.click('input[type="submit"]')
+
+        self.click_link("Update Your Listings")
+        self.click('input[type="radio"]')
+        self.click('input[type="submit"]')
+        
+        self.type("#description", "This is a comfy place with 5 beds 3 bath")
+        self.click('input[type="submit"]')
+        self.assert_element("#messages")
+        self.assert_text("Description updated successfully: " +
+                         "This is a comfy place with 5 beds 3 bath", 
+                         "#messages")
