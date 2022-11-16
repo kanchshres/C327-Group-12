53dff2aa685d114c9b99f1e7d6c3dfc8badf6e2c
Andrew Hunter <andrewhunter542@gmail.com>
2022-11-16 16:02:00 -0500

    finished test cases for preventing sql injection for the User.register() function.

--- qbay_test/injection_tests.py
+++ qbay_test/injection_tests.py
@@ -1,20 +1,41 @@
 import unittest
 from qbay.user import User
 
+"""
+This file defines the test cases for preventing SQL Injections
+"""
+
+
 class UnitTest(unittest.TestCase):
 
-    def test_username():
-        with open('./Generic_SQLI.txt') as f:
-            for line in f:
-                User.register(line, "testemail@gmail.com", "Password123!")
+    def test_username(self):
+        """ For each line/input/test-case, pass through the 
+        User.register() function as the username parameter to test for 
+        vulnerabilities to SQL Injection
+        """
 
-    def test_email():
-        with open('./Generic_SQLI.txt') as f:
+        with open("./qbay_test/Generic_SQLI.txt") as f:
+            for line in f:
+                User.register(line, "testemail@gmail.com", 
+                              "Password123!")
+
+    def test_email(self):
+        """ For each line/input/test-case, pass through the 
+        User.register() function as the email parameter to test for 
+        vulnerabilities to SQL Injection
+        """
+
+        with open('./qbay_test/Generic_SQLI.txt') as f:
             for line in f:
                 User.register("Bob", line, "Password123!")
 
-    def test_password():
-        with open('./Generic_SQLI.txt') as f:
+    def test_password(self):
+        """ For each line/input/test-case, pass through the 
+        User.register() function as the password parameter to test for 
+        vulnerabilities to SQL Injection
+        """
+
+        with open('./qbay_test/Generic_SQLI.txt') as f:
             for line in f:
                 User.register("Bob", "testemail@gmail.com", line)
                 
