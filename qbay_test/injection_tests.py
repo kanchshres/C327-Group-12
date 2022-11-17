d4f19c1f9049b07065cd1d0984130ed09cb238f1
KazSusilo <20ARS9@queensu.ca>
2022-11-16 19:05:50 -0500

    Update injection_tests.py
    Added create_listing test functions
    * create_account function assists with verifying the validity of the seller

--- qbay_test/injection_tests.py
+++ qbay_test/injection_tests.py
@@ -1,27 +1,103 @@
 import unittest
+from qbay.database import app, db
 from qbay.user import User
 from qbay.listing import Listing
 from datetime import datetime
 
+"""
+This file defines all SQL Injection Back-end Tests
+"""
+
+
 class UnitTest(unittest.TestCase):
+    def test_username(self):
+        """ For each line/input/test-case, pass through the 
+        User.register() function as the username parameter to test for 
+        vulnerabilities to SQL Injection
+        """
 
-    def test_username():
-        with open('./Generic_SQLI.txt') as f:
+        with open("./qbay_test/Generic_SQLI.txt") as f:
             for line in f:
-                User.register(line, "testemail@gmail.com", "Password123!")
+                User.register(line, "testemail@gmail.com", 
+                              "Password123!")
 
-    def test_email():
-        with open('./Generic_SQLI.txt') as f:
+    def test_email(self):
+        """ For each line/input/test-case, pass through the 
+        User.register() function as the email parameter to test for 
+        vulnerabilities to SQL Injection
+        """
+
+        with open('./qbay_test/Generic_SQLI.txt') as f:
             for line in f:
                 User.register("Bob", line, "Password123!")
 
-    def test_password():
-        with open('./Generic_SQLI.txt') as f:
+    def test_password(self):
+        """ For each line/input/test-case, pass through the 
+        User.register() function as the password parameter to test for 
+        vulnerabilities to SQL Injection
+        """
+
+        with open('./qbay_test/Generic_SQLI.txt') as f:
             for line in f:
                 User.register("Bob", "testemail@gmail.com", line)
+   
+    """Create account to verify in test_create_listing functions"""
+    def create_account(self, username, email, password):
+        # Create account 
+        User.register(username, email, password)
+        account = User.login(email, password)
+        return account
+        
+    def test_creat_listing_title(self):
+        """
+        For each line/input/test-case, pass through the Listing.create_listing
+        function as the title parameter to test for vulnerabilities
+        """
+        username, email, password = "TestCL1", "testCL1@gmail.com", "Onetwo!"
+        account = self.create_account(username, email, password)
 
-    def test_creat_listing_title():
-        with open('./Generic_SQLI.txt') as f:
+        with open('./qbay_test/Generic_SQLI.txt') as f:
+            t, d, p, o = "", "This is a lovely place", 100.00, account
             for line in f:
-                t, d, p, o = 1, 2, 3, line
-                Listing.create_listing(t, d, p, o)
+                t, d, p, o = line, d, p, o
+                try: 
+                    Listing.create_listing(t, d, p, o)
+                    raise ValueError(f"SECURITY BREACHED: {line}")
+                except (ValueError):
+                    self.assertRaises(ValueError)
+    
+    def test_create_listing_description(self):
+        """
+        For each line/input/test-case, pass through the Listing.create_listing
+        function as the description parameter to test for vulnerabilities
+        """
+        username, email, password = "TestCL2", "testCL2@gmail.com", "Onetwo!"
+        account = self.create_account(username, email, password)
+
+        with open('./qbay_test/Generic_SQLI.txt') as f:
+            t, d, p, o = "69 Ligma Road", "", 100.00, account
+            for line in f:
+                t, d, p, o = t, line, p, o
+                try: 
+                    Listing.create_listing(t, d, p, o)
+                    raise ValueError(f"SECURITY BREACHED: {line}")
+                except (ValueError):
+                    self.assertRaises(ValueError)
+    
+    def test_create_listing_price(self):
+        """
+        For each line/input/test-case, pass through the Listing.create_listing
+        function as the price parameter to test for vulnerabilities
+        """
+        username, email, password = "TestCL3", "testCL3@gmail.com", "Onetwo!"
+        account = self.create_account(username, email, password)
+
+        with open('./qbay_test/Generic_SQLI.txt') as f:
+            t, d, p, o = "69 Ligma Road", "This is a lovely place", "", account
+            for line in f:
+                t, d, p, o = t, d, line, o
+                try: 
+                    Listing.create_listing(t, d, p, o)
+                    raise ValueError(f"SECURITY BREACHED: {line}")
+                except (ValueError):
+                    self.assertRaises(ValueError)
