3bd4aff584a81442679fb1b8e7c5c43375c915a7
KazSusilo <20ARS9@queensu.ca>
2022-11-17 00:06:17 -0500

    Injection testing for create_listing 1/2
    * Tested injections on each parameter (title, description, price)
    * Created create_account to register and login account such that owner can be validated in create_listing
    * Created create_listing_helper to refactor similar code

--- qbay_test/injection_tests.py
+++ qbay_test/injection_tests.py
@@ -47,7 +47,38 @@ class UnitTest(unittest.TestCase):
         User.register(username, email, password)
         account = User.login(email, password)
         return account
-        
+
+    def create_listing_helper(self, t, d, p, o, param):
+        # Tests SQL Injection line on selected parameter
+        with open('./qbay_test/Generic_SQLI.txt') as f:
+            i = 1
+            for line in f:
+                # Determine which parameter to substitute for injection line
+                # Determine assertion statement based on parameter substituted
+                if param == "title":
+                    try:
+                        t, d, p, o = line, d, p, o
+                        Listing.create_listing(t, d, p, o)
+                        assert (Listing.valid_title(line)) is True
+                    except ValueError as e:
+                        assert (Listing.valid_title(line)) is False
+                elif param == "description":
+                    try:
+                        t, d, p, o = "Place " + str(i), line, p, o
+                        i += 1
+                        Listing.create_listing(t, d, p, o)
+                        assert (Listing.valid_description(line, t)) is True
+                    except ValueError as e:
+                        assert (Listing.valid_description(line, t)) is False
+                elif param == "price":
+                    try: 
+                        t, d, p, o = t, d, float(line), o
+                        Listing.create_listing(t, d, p, o)
+                        assert (Listing.valid_price(line)) is True
+                    except ValueError as e:
+                        # Could not convert string to float
+                        pass
+                
     def test_creat_listing_title(self):
         """
         For each line/input/test-case, pass through the Listing.create_listing
@@ -55,16 +86,9 @@ class UnitTest(unittest.TestCase):
         """
         username, email, password = "TestCL1", "testCL1@gmail.com", "Onetwo!"
         account = self.create_account(username, email, password)
-
-        with open('./qbay_test/Generic_SQLI.txt') as f:
-            t, d, p, o = "", "This is a lovely place", 100.00, account
-            for line in f:
-                t, d, p, o = line, d, p, o
-                try: 
-                    Listing.create_listing(t, d, p, o)
-                    raise ValueError(f"SECURITY BREACHED: {line}")
-                except (ValueError):
-                    self.assertRaises(ValueError)
+        title, description, price = "", "This is a lovely place", 100
+        owner, parameter = account, "title"
+        self.create_listing_helper(title, description, price, owner, parameter)
     
     def test_create_listing_description(self):
         """
@@ -73,16 +97,9 @@ class UnitTest(unittest.TestCase):
         """
         username, email, password = "TestCL2", "testCL2@gmail.com", "Onetwo!"
         account = self.create_account(username, email, password)
-
-        with open('./qbay_test/Generic_SQLI.txt') as f:
-            t, d, p, o = "69 Ligma Road", "", 100.00, account
-            for line in f:
-                t, d, p, o = t, line, p, o
-                try: 
-                    Listing.create_listing(t, d, p, o)
-                    raise ValueError(f"SECURITY BREACHED: {line}")
-                except (ValueError):
-                    self.assertRaises(ValueError)
+        title, description, price = "Place x", "", 100
+        owner, parameter = account, "description"
+        self.create_listing_helper(title, description, price, owner, parameter)
     
     def test_create_listing_price(self):
         """
@@ -91,13 +108,6 @@ class UnitTest(unittest.TestCase):
         """
         username, email, password = "TestCL3", "testCL3@gmail.com", "Onetwo!"
         account = self.create_account(username, email, password)
-
-        with open('./qbay_test/Generic_SQLI.txt') as f:
-            t, d, p, o = "69 Ligma Road", "This is a lovely place", "", account
-            for line in f:
-                t, d, p, o = t, d, line, o
-                try: 
-                    Listing.create_listing(t, d, p, o)
-                    raise ValueError(f"SECURITY BREACHED: {line}")
-                except (ValueError):
-                    self.assertRaises(ValueError)
+        title, description, price = "4 bed 2 bath", "This is a lovely place", 0
+        owner, parameter = account, "price"
+        self.create_listing_helper(title, description, price, owner, parameter)
